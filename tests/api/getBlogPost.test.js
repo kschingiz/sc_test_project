c7f4dacbd342a4920a693d6021a28eae327db989
Kengessov <kschingiz@gmail.com>
2020-01-07 19:59:24 +0600

    blog post handler tests refactoring

--- tests/api/getBlogPost.test.js
+++ tests/api/getBlogPost.test.js
@@ -4,12 +4,20 @@ import listen from "test-listen";
 import { apiResolver } from "next-server/dist/server/api-utils";
 import getBlogPostHandler from "../../pages/api/getBlogPost";
 
-test("should return blog post", async () => {
+import demoBlogPost from "../../src/demoData";
+
+function getServerWithHandler(handler) {
   const requestHandler = (req, res) => {
     return apiResolver(req, res, undefined, getBlogPostHandler);
   };
 
   const server = http.createServer(requestHandler);
+  return server;
+}
+
+test("should return blog post", async () => {
+  const server = getServerWithHandler(getBlogPostHandler);
+
   const url = await listen(server);
   const response = await fetch(url);
 
@@ -17,7 +25,7 @@ test("should return blog post", async () => {
 
   const { blogPost } = await response.json();
 
-  expect(blogPost.title).toBe("How to Find Great Speakers That Drive More Attendance");
+  expect(blogPost.title).toBe(demoBlogPost.title);
 
   return server.close();
 });
