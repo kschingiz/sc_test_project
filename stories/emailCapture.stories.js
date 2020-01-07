60eed8be62e96f0a0ce03e9ccaacddd47544ab60
Kengessov <kschingiz@gmail.com>
2020-01-07 18:14:14 +0600

    storybook setup and basic stories

---
+++ stories/emailCapture.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import EmailCapture from "../components/emailCapture";
+
+export default { title: "Email Capture" };
+
+export const defaultEmailCapture = () => <EmailCapture></EmailCapture>;
---
+++ stories/eventAppBar.stories.js
@@ -0,0 +1,6 @@
+import React from "react";
+import EventBar from "../components/eventBar";
+
+export default { title: "Event Bar" };
+
+export const defaultEventBar = () => <EventBar title="Event Bar"></EventBar>;
